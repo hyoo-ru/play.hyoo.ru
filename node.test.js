d27bb922138ed12b30316d44e857cf84214398da
nin-jin <sairi-na-tenshi@ya.ru>
2025-08-14 10:09:46 +0000

    Deploying to gh-pages from @ hyoo-ru/play.hyoo.ru@a552e57f4e15e492b168f40f7efe8b4d7bc58f2b 🚀

--- node.test.js
+++ node.test.js
@@ -12018,9 +12018,7 @@ var $;
                 return this.movies().get(id).genres().join(', ');
             }
             cover() {
-                const poster = this.movie_current()?.poster();
-                if (!poster)
-                    return '';
+                const poster = this.movie_current()?.poster() || 'https://habrastorage.org/webt/vd/jt/8d/vdjt8dluf8kkeav4ry3aowjx6ua.jpeg';
                 return `linear-gradient( #000000DF ), url( ${JSON.stringify(poster)} )`;
             }
             similars() {
